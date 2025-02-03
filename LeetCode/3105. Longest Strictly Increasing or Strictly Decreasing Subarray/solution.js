3389756ab29a5ddef5f28e28a0583cef1619a0bc
Yen-Chun,Lin <asd900606@gmail.com>
2025-02-03 21:00:55 +0800

    update: 3105. Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ LeetCode/3105. Longest Strictly Increasing or Strictly Decreasing Subarray/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestMonotonicSubarray = function (nums) {
+  let increase = 1;
+  let decrease = 1;
+  let maxLen = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      increase++;
+      decrease = 1;
+    } else if (nums[i] < nums[i - 1]) {
+      decrease++;
+      increase = 1;
+    } else {
+      increase = 1;
+      decrease = 1;
+    }
+    maxLen = Math.max(maxLen, increase, decrease);
+  }
+
+  return maxLen;
+};
+
+console.log(longestMonotonicSubarray([1, 10, 10]));
