3f2460e65608416dba5a88d695def867afe44279
Yen-Chun Lin <asd900606@gmail.com>
2025-10-05 04:52:30 +0800

    chore: update 11. Container With Most Water

---
+++ docs/LeetCode/11. Container With Most Water/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let mostWater = 0;
+
+  while (right > left) {
+    const waterCurrently =
+      Math.min(height[left], height[right]) * (right - left);
+
+    mostWater = Math.max(mostWater, waterCurrently);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return mostWater;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
