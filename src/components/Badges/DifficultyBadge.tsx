633f72276779e0da04ebba8f5c750f114ee9869d
Yen-Chun,Lin <asd900606@gmail.com>
2025-10-09 05:50:37 +0800

    feat: update with codewars kyu prop

--- src/components/Badges/DifficultyBadge.tsx
+++ src/components/Badges/DifficultyBadge.tsx
@@ -3,20 +3,39 @@ import Admonition from "@theme/Admonition";
 
 type Difficulty = "Easy" | "Medium" | "Hard";
 
-interface Props {
+interface DifficultyBadgeProps {
   difficulty?: Difficulty;
+  kyu?: number;
 }
 
-const DifficultyBadge: React.FC<Props> = ({ difficulty = "Easy" }) => {
-  const type =
-    difficulty === "Easy"
-      ? "tip"
-      : difficulty === "Medium"
-      ? "warning"
-      : "danger";
+const DifficultyBadge: React.FC<DifficultyBadgeProps> = ({
+  difficulty,
+  kyu,
+}) => {
+  let displayDifficulty = difficulty;
+
+  if (kyu) {
+    if (kyu >= 5 && kyu <= 8) displayDifficulty = "Easy";
+    else if (kyu >= 3 && kyu <= 4) displayDifficulty = "Medium";
+    else if (kyu >= 1 && kyu <= 2) displayDifficulty = "Hard";
+  }
+
+  const difficultyTypeMap = {
+    Easy: "tip",
+    Medium: "warning",
+    Hard: "danger",
+  };
+
+  const type = displayDifficulty ? difficultyTypeMap[displayDifficulty] : "tip";
+
+  const title = displayDifficulty
+    ? kyu
+      ? `${kyu} kyu`
+      : displayDifficulty
+    : "";
 
   return (
-    <Admonition type={type} icon={null} title={difficulty}>
+    <Admonition type={type} icon={null} title={title}>
       <></>
     </Admonition>
   );
